25c26b67b524b86e8847c85c64af26937dc4d5a1
Lalit Chowhan <lalit.chouhan8209@gmail.com>
2025-10-09 22:20:58 +0530

    Update Register.jsx

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -108,7 +108,7 @@ const Register = () => {
 
         <p className="text-center text-sm text-gray-600 mt-6">
           Already have an account?{' '}
-            <Link to="/login" className="text-red-600 hover:underline">
+            <a href="/login" className="text-red-600 hover:underline">
             Login here
           </a>
         </p>
